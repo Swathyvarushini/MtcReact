3bbe3cd841c3abc83091849ade06f3fb23234efb
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2024-08-15 12:39:10 +0530

    aws trial

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
-    URL:"https://mtcinspection.onrender.com",
+   // URL:"https://mtcinspection.onrender.com",
     // URL:"http://localhost:8080"
+      URL:"http://15.207.14.214:8080"
+
 };
 
 export default CONFIG;
