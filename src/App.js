fc994840df0b84c1fb895b47c9825aee3e5db6d4
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2024-10-08 13:44:39 +0530

    test build

--- src/App.js
+++ src/App.js
@@ -38,31 +38,31 @@ const App = () => {
 
 const AppRoutes = () => {
   const location = useLocation();
-  const isUserRoute = location.pathname.startsWith('/scanner') || location.pathname.startsWith('/form') || location.pathname.startsWith('/inspection-record') || location.pathname.startsWith('/security-record') || location.pathname.startsWith('/checker-record') || location.pathname.startsWith('/timekeeper-record');
-  const isAdminRoute = location.pathname.startsWith('/home') || location.pathname.startsWith('/user-details') || location.pathname.startsWith('/vehicle-details') || location.pathname.startsWith('/inspection-details') || location.pathname.startsWith('/security-details') || location.pathname.startsWith('checker-details') || location.pathname.startsWith('/timekeeper-details') || location.pathname.startsWith('/user-register') || location.pathname.startsWith('/vehicle-register');
+  const isUserRoute = location.pathname.startsWith('/scanner') || location.pathname.startsWith('/form') || location.pathname.startsWith('/inspection-record') || location.pathname.startsWith('/security-record') || location.pathname.startsWith('/checker-record');
+  const isAdminRoute = location.pathname.startsWith('/home') || location.pathname.startsWith('/user-details') || location.pathname.startsWith('/vehicle-details') || location.pathname.startsWith('/inspection-details') || location.pathname.startsWith('/security-details') || location.pathname.startsWith('checker-details') || location.pathname.startsWith('/user-register') || location.pathname.startsWith('/vehicle-register');
 
   return (
     <>
       {isUserRoute && <UserNavbar />}
       {isAdminRoute && <AdminNavbar />}
       <Routes>
-        <Route path="/" element={<ProtectedRoute><Login /></ProtectedRoute>} />
-        <Route path="/scanner" element={<ProtectedRoute><Scanner /></ProtectedRoute>} />
-        <Route path="/form" element={<ProtectedRoute><Form /></ProtectedRoute>} />
-        <Route path="/inspection-record" element={<ProtectedRoute><InspectionRecord /></ProtectedRoute>} />
-        <Route path="/security-record" element={<ProtectedRoute><SecurityRecord /></ProtectedRoute>} />
+        <Route path="/" element={<ProtectedRoute><Login/></ProtectedRoute>} />
+        <Route path="/scanner" element={<ProtectedRoute><Scanner/></ProtectedRoute>} />
+        <Route path="/form" element={<ProtectedRoute><Form/></ProtectedRoute>} />
+        <Route path="/inspection-record" element={<ProtectedRoute><InspectionRecord/></ProtectedRoute>} />
+        <Route path="/security-record" element={<ProtectedRoute><SecurityRecord/></ProtectedRoute>} />
         <Route path="/checker-record" element={<ProtectedRoute><CheckerRecord/></ProtectedRoute>} />
         {/* <Route path="/timekeeper-record" element={<ProtectedRoute><TimeKeeperRecord/></ProtectedRoute>} /> */}
-        <Route path="/select-role" element={<ProtectedRoute><SelectRole /></ProtectedRoute>} />
+        <Route path="/select-role" element={<ProtectedRoute><SelectRole/></ProtectedRoute>} />
         <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/user-details" element={<ProtectedRoute><UserDetails /></ProtectedRoute>} />
-        <Route path="/vehicle-details" element={<ProtectedRoute><VehicleDetails /></ProtectedRoute>} />
-        <Route path="/inspection-details" element={<ProtectedRoute><InspectionDetails /></ProtectedRoute>} />
-        <Route path="/security-details" element={<ProtectedRoute><SecurityDetails /></ProtectedRoute>} />
+        <Route path="/user-details" element={<ProtectedRoute><UserDetails/></ProtectedRoute>} />
+        <Route path="/vehicle-details" element={<ProtectedRoute><VehicleDetails/></ProtectedRoute>} />
+        <Route path="/inspection-details" element={<ProtectedRoute><InspectionDetails/></ProtectedRoute>} />
+        <Route path="/security-details" element={<ProtectedRoute><SecurityDetails/></ProtectedRoute>} />
         <Route path="/checker-details" element={<ProtectedRoute><CheckerDetails/></ProtectedRoute>} />
         {/* <Route path="/timekeeper-details" element={<ProtectedRoute><TimeKeeperDetails /></ProtectedRoute>} /> */}
-        <Route path="/user-register" element={<ProtectedRoute><UserRegister /></ProtectedRoute>} />
-        <Route path="/vehicle-register" element={<ProtectedRoute><VehicleRegister /></ProtectedRoute>} />
+        <Route path="/user-register" element={<ProtectedRoute><UserRegister/></ProtectedRoute>} />
+        <Route path="/vehicle-register" element={<ProtectedRoute><VehicleRegister/></ProtectedRoute>} />
       </Routes>
     </>
   );
