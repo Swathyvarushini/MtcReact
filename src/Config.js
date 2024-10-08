2e94cfba9cfd9389182148583d7bb61978bad379
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2024-10-08 11:03:04 +0530

    config change

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
    // URL:"https://mtcinspection.onrender.com",
-    URL:"http://localhost:8080"
+    //URL:"http://localhost:8080"
      // URL:"http://ec2-13-201-75-192.ap-south-1.compute.amazonaws.com:8080"
-    // URL:"https://caring-creation-production.up.railway.app"
+    URL:"https://caring-creation-production.up.railway.app"
     
 
 };
